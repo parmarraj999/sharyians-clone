373ef2c9dfec63d51054a2fd694e965fcd3b1365
parmarraj999 <rajraja5122@gmail.com>
2024-02-02 18:19:31 -0800

    complete

--- src/layouts/home/home.js
+++ src/layouts/home/home.js
@@ -1,23 +1,51 @@
 import React from 'react'
-import ButtonEmoji from "../../component/buttonEmoji";
 import "./home.css"
 import Button from '../../component/button/Button';
+import { motion } from 'framer-motion';
 
 function Home() {
   console.log("home")
+
   return (
-    <div className='main-container' >
-      <div className='home-container' >  
+    <div className='main-container'
+    >
+      <motion.div className='home-container'
+        animate={{ opacity: 1, background: "linear-gradient(180deg, #0d7ae599, transparent)" }}
+        initial={{ opacity: 0, background: "white" }}
+        transition={{ duration: .4, delay: 1.1 }}
+      >
         <div>
-          <h2>we are</h2>
-          <h1>web <span>d</span>eity</h1>
-          <h3>build <span>'n</span> infinity and beyond</h3>
+          <motion.h2
+            animate={{ y: "0px", opacity: 1 }}
+            initial={{ y: "-50px", opacity: 0 }}
+            transition={{ duration: .5, delay: 1.3 }}
+          >we are</motion.h2>
+          <motion.h1
+            animate={{ y: "0px", opacity: 1 }}
+            initial={{ y: "-50px", opacity: 0 }}
+            transition={{ duration: .5, delay: 1.5 }}
+          >web <span>d</span>eity</motion.h1>
+          <motion.h3
+            animate={{ opacity: 1 }}
+            initial={{ opacity: 0 }}
+            transition={{ duration: .5, delay: 1.7 }}
+          >build <span>'n</span> infinity and beyond</motion.h3>
         </div>
-        <Button Text="Let's Talk ->" color="#0d7ae5"/>
-        <div className='triple-icon-container'>
-          <img src='../../image/triple-down-icon.png'/>
-        </div>
-      </div>
+        <motion.div
+        animate={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
+        transition={{ duration: .5, delay: 1.9 }}
+        >
+          <Button Text="Let's Talk ->" color="#0d7ae5" />
+        </motion.div>
+        <motion.div className='triple-icon-container'
+        animate={{  opacity: 1 }}
+        initial={{  opacity: 0 }}
+        transition={{ duration: .5, delay: 2 }}
+        >
+          <img src='../../image/triple-down-icon.png' />
+        </motion.div>
+      </motion.div>
     </div>
   )
 }
--- src/layouts/nav/nav.js
+++ src/layouts/nav/nav.js
@@ -1,47 +1,94 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./nav.css"
 import { NavLink } from 'react-router-dom'
 import Button from '../../component/button/Button'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faClose } from '@fortawesome/free-solid-svg-icons'
 import MobileNav from '../../component/mobileNav/MobileNav'
+import { motion } from 'framer-motion'
 
 function Nav() {
 
   const [navToggle, setNavToggle] = useState(false);
 
   return (
-    <div className='nav-container' >
-      <div className='nav' >
-        <img src="../../image/logo.png" />
+    <motion.div className='nav-container'
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      transition={{ duration: .5 }}
+    >
+      <div className='nav'>
+        <motion.div
+          animate={{ y: "0px", opacity: 1 }}
+          initial={{ y: "-50px", opacity: 0 }}
+          transition={{ duration: .5, delay: 0.5 }}
+        >
+          <img src="../../image/logo.png" />
+        </motion.div>
         <ul>
-          <NavLink className="navlink" >Cards</NavLink>
-          <NavLink className="navlink" > <span></span> Work</NavLink>
-          <NavLink className="navlink" > <span></span> Pricing</NavLink>
-          <NavLink className="navlink" > <span></span> Contact</NavLink>
+          <motion.div
+            animate={{ y: "0px", opacity: 1 }}
+            initial={{ y: "-50px", opacity: 0 }}
+            transition={{ duration: .5, delay: 0.8 }}
+          >
+            <NavLink className="navlink" >Cards</NavLink>
+          </motion.div>
+          <motion.div
+            animate={{ y: "0px", opacity: 1 }}
+            initial={{ y: "-50px", opacity: 0 }}
+            transition={{ duration: .5, delay: 0.9 }}
+          >
+            <NavLink className="navlink" > <span></span> Work</NavLink>
+          </motion.div>
+          <motion.div
+            animate={{ y: "0px", opacity: 1 }}
+            initial={{ y: "-50px", opacity: 0 }}
+            transition={{ duration: .5, delay: 1 }}
+          >
+            <NavLink className="navlink" > <span></span> Pricing</NavLink>
+          </motion.div>
+          <motion.div
+            animate={{ y: "0px", opacity: 1 }}
+            initial={{ y: "-50px", opacity: 0 }}
+            transition={{ duration: .5, delay: 1.1 }}
+          >
+            <NavLink className="navlink" > <span></span> Contact</NavLink>
+          </motion.div>
         </ul>
-        <div style={{ display: "flex", gap: ".8rem", alignItems: "center"}} >
+        <div style={{ display: "flex", gap: ".8rem", alignItems: "center" }} >
           {
-            navToggle ?""
-            : 
-            <Button link="Contact" Text="Book A Call" />
+            navToggle ? ""
+              :
+              <motion.div
+                animate={{ y: "0px", opacity: 1 }}
+                initial={{ y: "-50px", opacity: 0 }}
+                transition={{ duration: .5, delay: 0.6 }}
+              >
+                <Button link="Contact" Text="Book A Call" />
+              </motion.div>
           }
           {
             navToggle ?
-              <FontAwesomeIcon className='menu-icon' icon={faClose} style={{ fontSize: "1.8rem", color: "white" }} onClick={()=>setNavToggle(false)} />
+              <FontAwesomeIcon className='menu-icon' icon={faClose} style={{ fontSize: "1.8rem", color: "white" }} onClick={() => setNavToggle(false)} />
               :
               <>
-                <FontAwesomeIcon className='menu-icon' icon={faBars} style={{ fontSize: "1.8rem", color: "white" }} onClick={()=>setNavToggle(true)}/>
+                <motion.div
+                  animate={{ y: "0px", opacity: 1 }}
+                  initial={{ y: "-50px", opacity: 0 }}
+                  transition={{ duration: .5, delay: .7 }}
+                >
+                  <FontAwesomeIcon className='menu-icon' icon={faBars} style={{ fontSize: "1.8rem", color: "white" }} onClick={() => setNavToggle(true)} />
+                </motion.div>
               </>
           }
         </div>
         {
-          navToggle ? 
-           <MobileNav/>
-           : ""
+          navToggle ?
+            <MobileNav />
+            : ""
         }
       </div>
-    </div>
+    </motion.div>
   )
 }
 
